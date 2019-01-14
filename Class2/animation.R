d9d441cce9af0931499925abad3084b4b2766fed
rodazuero <rodazuero@gmail.com>
2019-01-14 09:20:13 -0500

    Up to Parallelism

--- Class2/animation.R
+++ Class2/animation.R
@@ -9,7 +9,6 @@ saveHTML({
   df<-function(x){2*x+2}
   x0=3
   fxe<-function(x){x^2+2*x+2}
-  fxeVector<-fx*0
   y[1]=x0
   for (i in 1:20) {
     plot(xguess,y, ylim = c(0.8, 18),xlim=c(-3,3),col="blue")
@@ -21,4 +20,31 @@ saveHTML({
   }
 }, img.name = "Grad_Descent", imgdir = "Grad_descent", htmlfile = "Grad_descent.html", 
 autobrowse = FALSE, title = "Demo of 20 grad descent")
+
+
+
+
+
+
+
+x<- seq(-3,3,0.1)
+L<-length(x)
+xguess <- rep(NA, L)
+fx=x^2+2*x+2
+fxe<-function(x){x^2+2*x+2}
+df<-function(x){2*x+2}
+x0=3
+alpha=0.1
+
+
+for (i in 1:L) {
+  xguess[i]=x0
+  y[i]=fxe(x0)
+  x0=x0-alpha*df(x0)
+}
+
+
+plot(xguess,y, ylim = c(0.8, 18),xlim=c(-3,3),col="blue")
+lines(x,fx,col='green')
+
 
